eea1c8999273ba2cb3b44dc505836f7f6e5e0cdf
RitikaV03 <rtkvardhan000@gmail.com>
2021-08-03 16:55:20 +0530

    commit by RitikaV03

--- src/test/java/com/cg/project/stepdefs/LoginStepDef.java
+++ src/test/java/com/cg/project/stepdefs/LoginStepDef.java
@@ -24,7 +24,7 @@ public class LoginStepDef {
 	public void setupScenarioEnv() {
 		driver = DriverFactory.getDriver();
                 driver.manage().window().maximize();
-                driver.manage().timeouts.implicitlywait(20,TIMEUNIT.Seconds);
+                driver.manage().timeouts.implicitlywait(20,TIMEUNIT.Seconds)
 		pageBean = PageFactory.initElements(driver, LoginPageBean.class);
 	}
 
